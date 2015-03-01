f4630398c4e25c004b2bfd9e8d7de75d119973f7
HASEGAWA Tomoki <tomoki@dgcircus.com>
2015-03-01 15:23:56 +0900

    Chapter 3-2

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -1,25 +1,41 @@
-//
-//  ViewController.swift
-//  SimpleBrowser
-//
-//  Created by 長谷川 智希 on 2015/02/28.
-//  Copyright (c) 2015年 Digital Circus Inc. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var webView: UIWebView!
+	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var backButton: UIBarButtonItem!
+	@IBOutlet weak var reloadButton: UIBarButtonItem!
+	@IBOutlet weak var stopButton: UIBarButtonItem!
+	
+	// ホームページのURL。起動時にこのページを開く。
+	let homeUrl = "http://www.yahoo.co.jp"
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		// ホームページを開く。
+		openUrl(homeUrl)
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	// 文字列で指定されたURLをWeb Viewで開く。
+	func openUrl(urlString: String){
+		let url = NSURL(string: urlString)
+		let urlRequest = NSURLRequest(URL: url!)
+		webView.loadRequest(urlRequest)
+	}
+	
+	// MARK: - IBAction
+	@IBAction func backButtonTapped(sender: UIBarButtonItem) {
+		webView.goBack()
+	}
+	@IBAction func reloadButtonTapped(sender: UIBarButtonItem) {
+		webView.reload()
+	}
+	@IBAction func stopButtonTapped(sender: UIBarButtonItem) {
+		webView.stopLoading()
+	}
 }
 
